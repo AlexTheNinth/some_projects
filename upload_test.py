276f0ca30476d2ab4b62d80d56594524a9a4c427
AlexTheNinth <118676528+AlexTheNinth@users.noreply.github.com>
2022-11-20 16:17:27 +0100

    Add files via upload

---
+++ upload_test.py
@@ -0,0 +1,8 @@
+# Upload test to github
+print("""
+Hello World! If you are reading this, that means it was
+successfully uploaded to Github and you are running a Python file.
+
+Best of luck
+\t Alex
+""")
